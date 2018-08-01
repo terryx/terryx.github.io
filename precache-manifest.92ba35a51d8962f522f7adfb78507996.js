bd810c88ec3514f9e6745dad0fdb44c9df58ed2c
Terry Yuen <terryxlife@gmail.com>
2018-08-01 16:34:27 +0800

    Add noopener link

---
+++ precache-manifest.92ba35a51d8962f522f7adfb78507996.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "d374c4bfe8e113964b9a",
+    "url": "/js/chunk-vendors.d374c4bf.js"
+  },
+  {
+    "revision": "46ab47d7893054ecdd7f",
+    "url": "/js/app.46ab47d7.js"
+  },
+  {
+    "revision": "b45fdf8d5db35173d0bfe69b1353c14f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "46ab47d7893054ecdd7f",
+    "url": "/css/app.1fee125f.css"
+  }
+];
